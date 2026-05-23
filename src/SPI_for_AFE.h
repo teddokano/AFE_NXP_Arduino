45651146d314eba0d6a9d1dd246d30b35028f343
Tedd OKANO <okano.akifumi@gmail.com>
2026-05-23 09:33:17 +0900

    all public methods in SPI_for_AFE.h are made virtual. Allowing to be overriden by inherited classes

--- src/SPI_for_AFE.h
+++ src/SPI_for_AFE.h
@@ -27,43 +27,43 @@ public:
 	 * @param data pointer to data buffer
 	 * @param size data size
 	 */
-	void txrx( uint8_t *data, int size );
+	virtual void txrx( uint8_t *data, int size );
 
 	/** Register write, 8 bit
 	 *
 	 * @param reg register index
 	 */
-	void write_r16( uint16_t reg );
+	virtual void write_r16( uint16_t reg );
 
 	/** Register write, 16 bit
 	 *
 	 * @param reg register index
 	 * @param val data value
 	 */
-	void write_r16( uint16_t reg, uint16_t val );
+	virtual void write_r16( uint16_t reg, uint16_t val );
 
 	/** Register read, 16 bit
 	 *
 	 * @param reg register index
 	 * @return data value
 	 */
-	uint16_t read_r16( uint16_t reg );
+	virtual uint16_t read_r16( uint16_t reg );
 
 	/** Register write, 24 bit
 	 *
 	 * @param reg register index
 	 * @param val data value
 	 */
-	void write_r24( uint16_t reg, uint32_t val );
+	virtual void write_r24( uint16_t reg, uint32_t val );
 
 	/** Register read, 24 bit
 	 *
 	 * @param reg register index
 	 * @return data value
 	 */
-	int32_t read_r24( uint16_t reg );
+	virtual int32_t read_r24( uint16_t reg );
 
-	void burst( uint32_t *data, int length, int width = 3 );
+	virtual void burst( uint32_t *data, int length, int width = 3 );
 
 protected:
 	void init( void );
